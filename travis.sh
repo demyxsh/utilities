d3dbd7b9c1a0859f24ef9fdf9a061b1390c5e85f
Demyx <info@demyx.sh>
2021-03-08 22:35:40 -0800

    Misc update

--- travis.sh
+++ travis.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # Demyx
 # https://demyx.sh
-# https://github.com/peter-evans/dockerhub-description/blob/master/entrypoint.sh
-IFS=$'\n\t'
 
 # Get versions
-DEMYX_UTILITIES_DEBIAN_VERSION=$(docker exec -t "$DEMYX_REPOSITORY" cat /etc/debian_version | sed -e 's/\r//g')
+DEMYX_UTILITIES_DEBIAN_VERSION=$(docker exec -t "$DEMYX_REPOSITORY" cat /etc/debian_version | sed 's/\r//g')
 
 # Replace versions
 sed -i "s|debian-.*.-informational|debian-${DEMYX_UTILITIES_DEBIAN_VERSION}-informational|g" README.md
@@ -14,33 +12,23 @@ sed -i "s|debian-.*.-informational|debian-${DEMYX_UTILITIES_DEBIAN_VERSION}-info
 echo "DEMYX_UTILITIES_DEBIAN_VERSION=$DEMYX_UTILITIES_DEBIAN_VERSION" > VERSION
 
 # Push back to GitHub
-git config --global user.email "travis@travis-ci.org"
+git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
-git remote set-url origin https://${DEMYX_GITHUB_TOKEN}@github.com/demyxco/"$DEMYX_REPOSITORY".git
-# Commit VERSION first
+git remote set-url origin https://"$DEMYX_GITHUB_TOKEN"@github.com/demyxco/"$DEMYX_REPOSITORY".git
+# Push VERSION file first
 git add VERSION
-git commit -m "DEBIAN $DEMYX_UTILITIES_DEBIAN_VERSION"
+git commit -m "ALPINE $DEMYX_NGINX_ALPINE_VERSION, NGINX $DEMYX_NGINX_VERSION"
 git push origin HEAD:master
-# Commit the rest
+# Add and commit the rest
 git add .
 git commit -m "Travis Build $TRAVIS_BUILD_NUMBER"
 git push origin HEAD:master
 
-# Set the default path to README.md
-README_FILEPATH="./README.md"
-
-# Acquire a token for the Docker Hub API
-echo "Acquiring token"
-TOKEN="$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'$DEMYX_USERNAME'", "password": "'$DEMYX_PASSWORD'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)"
-
 # Send a PATCH request to update the description of the repository
 echo "Sending PATCH request"
-REPO_URL="https://hub.docker.com/v2/repositories/${DEMYX_USERNAME}/${DEMYX_REPOSITORY}/"
-RESPONSE_CODE=$(curl -s --write-out %{response_code} --output /dev/null -H "Authorization: JWT ${TOKEN}" -X PATCH --data-urlencode full_description@${README_FILEPATH} ${REPO_URL})
-echo "Received response code: $RESPONSE_CODE"
+DEMYX_DOCKER_TOKEN="$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"$DEMYX_USERNAME"'", "password": "'"$DEMYX_PASSWORD"'"}' "https://hub.docker.com/v2/users/login/" | jq -r .token)"
+DEMYX_RESPONSE_CODE="$(curl -s --write-out "%{response_code}" --output /dev/null -H "Authorization: JWT ${DEMYX_DOCKER_TOKEN}" -X PATCH --data-urlencode full_description@"README.md" "https://hub.docker.com/v2/repositories/${DEMYX_USERNAME}/${DEMYX_REPOSITORY}/")"
+echo "Received response code: $DEMYX_RESPONSE_CODE"
 
-if [ $RESPONSE_CODE -eq 200 ]; then
-  exit 0
-else
-  exit 1
-fi
+# Return an exit 1 code if response isn't 200
+[[ "$DEMYX_RESPONSE_CODE" != 200 ]] && exit 1
